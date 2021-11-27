6c7f6271b9c61f141e5ed566974d412e42b79d95
jaiminmoradiya <74696622+jaiminmoradiya@users.noreply.github.com>
2021-11-27 21:18:14 +0530

    Add files via upload

---
+++ GST/hh.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Document</title>
+    <link rel="stylesheet" href="navbar.css">
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="home.php">Goods And Services Tax</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item" id="1">
+                    <a class="nav-link" href="credit leadger.php">Credit Leadger</a>
+                </li>
+                <li class="nav-item" id="1">
+                    <a class="nav-link" href="invoice.php">Invoice</a>
+                </li>
+                <li class="nav-item" id="1">
+                    <a class="nav-link" href="return.php">Returns</a>
+                </li>
+                <li class="nav-item" id="1">
+                    <a class="nav-link" href="product.php">Product</a>
+                </li>
+                <li class="nav-item" id="1">
+                    <a class="nav-link" href="profile.php">Profile</a>
+                </li>
+                
+                
+            </ul>
+            <ul>
+            
+            <a href="login.php" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-log-out"></span> Log out
+        </a>   
+            </ul>
+            
+        </div>
+    </nav>
+</body>
+
+</html>
